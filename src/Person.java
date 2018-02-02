b3e55beaa38ea8342b4b61f22913997d63f5c2f0
Nirbhay Vig <19vign@elmbrookstudents.org>
2018-02-02 11:08:21 -0600

    Worked on both Person and Projects

--- src/Person.java
+++ src/Person.java
@@ -1,12 +1,14 @@
 
 public class Person {
-private String name;
-private double salary;
-public Person() {
-	name="bob";
-	salary=0;
-}
-public void moreMoney() {
-	salary+=1000;
-}
+	private String name;
+	private double salary;
+
+	public Person() {
+		name = "bob";
+		salary = 0;
+	}
+
+	public void moreMoney() {
+		salary += 1000;
+	}
 }
--- src/Projects.java
+++ src/Projects.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
 
 public class Projects {
+	private ArrayList<String> completedProjects;
+	private String temp;
 
+	public Projects() {
+		completedProjects = new ArrayList<String>();
+		temp = null;
+	}
+	
+	public String projectSummary() {
+		for(int i = 0; i < completedProjects.size() ; i++) {
+			temp = temp + completedProjects.get(i) + "\n";
+		}
+		return temp;
+	}
+	
+	public void addProject(String x) {
+		completedProjects.add(x);
+	}
+	
 }

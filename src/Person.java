6e0e322a3e825a83ed3e0603b711fc951624c8f0
Nirbhay Vig <19vign@elmbrookstudents.org>
2018-02-02 11:17:06 -0600

    Finished Feature

--- src/Person.java
+++ src/Person.java
@@ -2,13 +2,23 @@
 public class Person {
 	private String name;
 	private double salary;
-
+	private Projects completed;
+	
 	public Person() {
 		name = "bob";
 		salary = 0;
+		completed = new Projects();
 	}
 
 	public void moreMoney() {
 		salary += 1000;
+	}
+	
+	public void addProject(String x) {
+		completed.addProject(x);
+	}
+	
+	public String completedProjects() {
+		return completed.projectSummary();
 	}
 }
